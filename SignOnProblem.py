3d8edc1d34ec434b0efd25fd6d831005f8a2a3aa
Jessie Jones <Jessie.Jones14@Protonmail.com>
2025-01-23 16:22:43 -0500

    updated failure tracking

--- SignOnProblem.py
+++ SignOnProblem.py
@@ -34,72 +34,120 @@ def processLogs(logs, maxSpan):
     
     for i in range(len(logs)-1):
         log_entry= logs[i].split()
+        condition1 = len(log_entry) == 3
+        
+        # ensures the log entry has three parts a userID a timestamp and a 'sign-on' or 'sign-off'
+        if not condition1:
+            print(f"Condition 1 skip {log_entry}")
+            continue
         user_name = log_entry[0]
         user_name_next = logs[i+1].split()[0]
         user_name_prev = logs[i-1].split()[0]
         time_stamp = int(log_entry[1])
+
+        # ensures that once sorted//which should be 
+        # put sign-on and ahead of sign-off with user_id's 
+        # equal, it cancels if it gets to an index where 
+        # the next is just a single digit.
+        condition2 = len(logs[i+1].split()) ==3
+        if not condition2:
+            print(f"Condition 2 skip {logs[i+1].split()}")
+            continue
+
         time_stamp2 = int(logs[i+1].split()[1])
         action = log_entry[2]
+
         action_count = 0
-        deleteme = False
+        delete_me = False
         cause_code =""
         
-        condition1 = len(log_entry) == 3
-        condition2 = type(pattern.fullmatch(user_name)) != None
-        condition3 = len(user_name) <= 9
-        condition4 = list(str(time_stamp))[0] != "0"
-        condition5 = 0 < time_stamp <= 10**9
-        condition6 = action == "sign-in" or action == "sign-out"
-        condition7 = time_stamp2 > time_stamp
+       #now that we have variables defined we can process
+        condition3= type(pattern.fullmatch(user_name)) != None
+        condition4= len(user_name) <= 9
+        condition5= list(str(time_stamp))[0] != "0"
+        condition6= time_stamp2 > time_stamp
+        condition7= 0 < time_stamp <= 10**9
+        condition8= action == "sign-in" or action == "sign-out"
+        condition9= user_name == user_name_next and user_name != user_name_prev
+        condition10= 1<= maxSpan
+        condition11= maxSpan <= 10**5
 
+        if not condition3:
+            print(f" Condition 3 Failure: {log_entry}")
+            continue
+        if not condition4:
+            print(f" Condition 4 Failure: {log_entry}")
+            continue
+        if not condition5:
+            print(f" Condition 5 Failure: {log_entry}")
+            continue
+        if not condition6:
+            print(f" Condition 6 Failure: {log_entry}")
+            continue
+        if not condition7:
+            print(f" Condition 7 Failure: {log_entry}")
+            continue
+        if not condition8:
+            print(f" Condition 8 Failure: {log_entry}")
+            continue
+        if not condition9:
+            print(f" Condition 9 Failure: {log_entry}")
+            continue
+        if not condition10:
+            print(f" Condition 10 Failure: {log_entry}")
+            continue
+        if not condition11:
+            print(f" Condition 11 Failure: {log_entry}")
+            continue
+    
         
-        #Handle USER ID
-        if pattern.fullmatch(user_name) is not None:
-            if len(user_name) <=9:
-                #handle timestamp
-                if pattern.fullmatch(str(time_stamp)) is not None:
-                    if list(str(time_stamp))[0] != "0":
-                        if 0 < time_stamp <= 10**9:
-                            #handle action statment
-                            if action == "sign-in" or action == "sign-out":
-                                if user_name == user_name_next and user_name != user_name_prev:
-                                    if time_stamp2 > time_stamp:
+    #     #Handle USER ID
+    #     if pattern.fullmatch(user_name) is not None:
+    #         if len(user_name) <=9:
+    #             #handle timestamp
+    #             if pattern.fullmatch(str(time_stamp)) is not None:
+    #                 if list(str(time_stamp))[0] != "0":
+    #                     if 0 < time_stamp <= 10**9:
+    #                         #handle action statment
+    #                         if action == "sign-in" or action == "sign-out":
+    #                             if user_name == user_name_next and user_name != user_name_prev:
+    #                                 if time_stamp2 > time_stamp:
                                         
-                                        diff_num = int(logs[i+1].split()[1])- int(logs[i].split()[1])
-                                        if 1 <= diff_num <= 10**5:
+    #                                     diff_num = int(logs[i+1].split()[1])- int(logs[i].split()[1])
+    #                                     if 1 <= diff_num <= 10**5:
                                         
-                                            results_dict = {user_name: diff_num}
-                                    else:
-                                        deleteme = True
-                                        cause_code = "Sign out time stamp larger than sign in"
-                                else:
-                                    deleteme = True
-                                    cause_code = "More than 1 user entry"
-                                # get MaxSpan
-                                # apply condition  1 
-                                # apply condition 2
-                            else:
-                                deleteme = True
-                                cause_code = "Sign-In / Sign-Out Wrong Action"
-                        else:
-                            deleteme = True
-                            cause_code = "time stamp value out of range"
-                    else:
-                        deleteme = True
-                        cause_code = "time stamp starts with zero"
-                else:
-                    deleteme = True
-                    cause_code ="time stamp has letters or symbols included"
-            else:
-                deleteme = True
-                cause_code = "User ID has too many digits"
-        else:
-            deleteme = True
-            cause_code = "UserID is alpha numeric or contains symbols"
+    #                                         results_dict = {user_name: diff_num}
+    #                                 else:
+    #                                     deleteme = True
+    #                                     cause_code = "Sign out time stamp larger than sign in"
+    #                             else:
+    #                                 deleteme = True
+    #                                 cause_code = "More than 1 user entry"
+    #                             # get MaxSpan
+    #                             # apply condition  1 
+    #                             # apply condition 2
+    #                         else:
+    #                             deleteme = True
+    #                             cause_code = "Sign-In / Sign-Out Wrong Action"
+    #                     else:
+    #                         deleteme = True
+    #                         cause_code = "time stamp value out of range"
+    #                 else:
+    #                     deleteme = True
+    #                     cause_code = "time stamp starts with zero"
+    #             else:
+    #                 deleteme = True
+    #                 cause_code ="time stamp has letters or symbols included"
+    #         else:
+    #             deleteme = True
+    #             cause_code = "User ID has too many digits"
+    #     else:
+    #         deleteme = True
+    #         cause_code = "UserID is alpha numeric or contains symbols"
                             
-    for k in results_dict.keys():
-        results = list(results_dict.keys())
-    return results_dict
+    # for k in results_dict.keys():
+    #     results = list(results_dict.keys())
+    # return results_dict
             
 processLogs(arr, 1)
    
